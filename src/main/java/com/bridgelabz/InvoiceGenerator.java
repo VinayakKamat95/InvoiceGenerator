391767ff8e030cda6e1885d83bb8a89349079e6e
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-05-12 19:50:52 +0530

    Added : Calculate Total Fare and check for multiple rides

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -10,4 +10,13 @@ public class InvoiceGenerator {
         double totalFare = distance_inKM * MINIMUM_COST_PER_KM + time_inMin * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
     }
+
+    /*calculateFare() method takes Ride[] Array return totalFare*/
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for(Ride ride : rides){
+            totalFare += this.calculateFare(ride.distance,ride.time);
+        }
+        return totalFare;
+    }
 }
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -4,13 +4,24 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
-    /*Test case to check calculateFare() returns expected totalFare*/
+
+    /*Test case to check calculateFare() given Distance And Time Should Return TotalFare*/
     @Test
     public void givenDistanceAndTimeShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        double distance_InKM = 2;
+        double distance_InKM = 2.0;
         int time_InMin = 5;
         double totalFare = invoiceGenerator.calculateFare(distance_InKM,time_InMin);
         Assert.assertEquals(25,totalFare,0.0);
+    }
+
+    /*Test case to check calculateFare() given Multiple Rides Should Return TotalFare*/
+    @Test
+    void givenMultipleRides_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0,5),
+                new Ride(0.1,1)};
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30,fare,0.0);
     }
 }
